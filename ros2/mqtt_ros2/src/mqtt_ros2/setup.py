1f586bb444db8eec366870717e6d58efc9c04237
natxozm13 <natxozm13@Ubuntu20.myguest.virtualbox.org>
2024-11-27 11:30:35 +0100

    Nodo ROS2

--- ros2/mqtt_ros2/src/mqtt_ros2/setup.py
+++ ros2/mqtt_ros2/src/mqtt_ros2/setup.py
@@ -7,8 +7,7 @@ setup(
     version='1.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name + '/config', ['config/speed_provider_config.yaml']),
         ('share/' + package_name + '/launch', ['launch/speed_provider.launch.py']),
         ('share/' + package_name, ['package.xml']),
